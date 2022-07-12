7e8b37bdca9130915de16aaf05338b68f0a71116
ori davidi <oridavidi100@gmail.com>
2022-07-12 13:37:00 +0300

    clean

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,22 +1,19 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import App from './components/App';
+import './components/style/index.css';
+import rootReducer from './reducer/rootReducer';
+import { configureStore } from '@reduxjs/toolkit';
+const store = configureStore({ reducer: rootReducer });
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
-    <App />
-    {/* </Provider> */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
