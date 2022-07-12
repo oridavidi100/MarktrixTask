7566a3f11838699464c498c4628d17ba61a01ef4
ori davidi <oridavidi100@gmail.com>
2022-07-12 12:36:35 +0300

    the index
    the index that runs all the backend

--- backEnd/src/index.ts
+++ backEnd/src/index.ts
@@ -1,10 +1,20 @@
 import express from 'express';
-// import cors from 'cors';
-const port = 5000;
+import cors from 'cors';
 export const app = express();
-app.get('/', (req, res) => {
-  res.send('hell0');
-});
+const port = 5000;
+import Router from './routes/name';
+import errorHandlerMiddleware from './midllewares/errorHandler';
+
+app.use(cors());
+
+app.use('/api', Router);
+
+// app.get('/', (req, res) => {
+//   res.send('hell0');
+// });
+
+app.use(errorHandlerMiddleware);
+
 app.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`);
 });
