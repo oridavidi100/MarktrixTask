d3ae9aa0cfba91499037068a42676b1039552bd4
ori davidi <oridavidi100@gmail.com>
2022-07-12 17:25:46 +0300

    reducer ste events

--- client/src/reducer/actions/action.ts
+++ client/src/reducer/actions/action.ts
@@ -1,5 +1,15 @@
 import { Data } from '../../@types/types';
 
-export const setEvents = (lastSearch: Data.LastSearch) => {
+export const setEvents = (name: string, response: Data.Response) => {
+  const lastSearch: Data.LastSearch = {
+    name: name,
+    gender: response.probabilityGender,
+    loaction: response.probabilityLocation,
+  };
   return { type: 'ADD_SEARCH', payload: lastSearch };
+};
+
+export const setState = (searchHistory: any) => {
+  const history = JSON.parse(searchHistory);
+  return { type: 'SET_STATE', payload: history };
 };
